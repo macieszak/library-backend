4252d092b3fa14ce80969a9bb5ad537c7d2888b8
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-09-25 19:26:50 +0200

    Read only configuration for Book class

---
+++ spring-boot-library/src/main/java/com/maciejmaksymiuk/springbootlibrary/config/MyDataRestConfig.java
@@ -0,0 +1,36 @@
+package com.maciejmaksymiuk.springbootlibrary.config;
+
+import com.maciejmaksymiuk.springbootlibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private String theAllowedOrigins = "http://localhost:3000";
+
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration configuration, CorsRegistry corsRegistry) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.PUT, HttpMethod.DELETE};
+        configuration.exposeIdsFor(Book.class);
+        dissableHttpMethods(Book.class, configuration, theUnsupportedActions);
+
+        // Configure CORS Mapping
+        corsRegistry.addMapping(theAllowedOrigins);
+    }
+
+    private void dissableHttpMethods(Class theClass, RepositoryRestConfiguration configuration, HttpMethod[] theUnsupportedActions) {
+        configuration.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure(((metdata, httpMethods) ->
+                        httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) ->
+                        httpMethods.disable(theUnsupportedActions)));
+    }
+
+}
