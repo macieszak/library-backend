5b91f31c403d6ba71e3d5209bbe0ab08800e1119
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-12-01 23:42:01 +0100

    Added findByTitleContaing method.

--- spring-boot-library/src/main/java/com/maciejmaksymiuk/springbootlibrary/repository/BookRepository.java
+++ spring-boot-library/src/main/java/com/maciejmaksymiuk/springbootlibrary/repository/BookRepository.java
@@ -1,8 +1,16 @@
 package com.maciejmaksymiuk.springbootlibrary.repository;
 
 import com.maciejmaksymiuk.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 
 }
