6639708930556ac9275291fa94d2811b89270c4e
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-12-04 10:48:48 +0100

    Added checkout repository.

--- spring-boot-library/src/main/java/com/maciejmaksymiuk/springbootlibrary/repository/CheckoutRepository.java
+++ spring-boot-library/src/main/java/com/maciejmaksymiuk/springbootlibrary/repository/CheckoutRepository.java
@@ -4,11 +4,13 @@ import com.maciejmaksymiuk.springbootlibrary.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
 
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
 
-
+    List<Checkout> findBooksByUserEmail(String userEmail);
 
 }
